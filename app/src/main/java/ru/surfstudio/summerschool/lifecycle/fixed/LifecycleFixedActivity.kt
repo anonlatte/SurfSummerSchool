8574381c2c4daa44ea39a10f31bfb5488d88cc37
German Proshunin <g.proshunin15@gmail.com>
2022-06-30 15:39:51 +0500

    Доработка логов

--- app/src/main/java/ru/surfstudio/summerschool/lifecycle/fixed/LifecycleFixedActivity.kt
+++ app/src/main/java/ru/surfstudio/summerschool/lifecycle/fixed/LifecycleFixedActivity.kt
@@ -1,7 +1,11 @@
 package ru.surfstudio.summerschool.lifecycle.fixed
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
+import androidx.core.view.isVisible
 import ru.surfstudio.summerschool.R
 import ru.surfstudio.summerschool.databinding.ActivityExampleLifecycleBinding
 
@@ -15,15 +19,48 @@ class LifecycleFixedActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityExampleLifecycleBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        logEvent("onCreate")
+        initViews()
+        // finish()
+    }
 
-        setTitle(R.string.title_lifecycle_fixed)
+    private fun logEvent(event: String) {
+        if (logsEnabled()) {
+            Toast.makeText(this, event, Toast.LENGTH_SHORT).show()
+        }
+    }
 
-        updateText()
-        binding.btnClick.setOnClickListener {
-            counter++
-            updateText()
+    override fun onStart() {
+        super.onStart()
+        logEvent("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        logEvent("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        logEvent("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        logEvent("onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        logEvent("restarting")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (isFinishing) {
+            logEvent("onDestroy")
+        } else {
+            logEvent("onRestart")
         }
     }
 
@@ -37,7 +74,38 @@ class LifecycleFixedActivity : AppCompatActivity() {
         super.onSaveInstanceState(outState)
     }
 
+    private fun initViews() {
+        binding = ActivityExampleLifecycleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setTitle(R.string.title_lifecycle_fixed)
+
+        updateText()
+        binding.btnClick.setOnClickListener {
+            counter++
+            updateText()
+
+        }
+        binding.switchLogs.isVisible = true
+        binding.switchLogs.isChecked = logsEnabled()
+        binding.switchLogs.setOnClickListener {
+            updateLogs((it as SwitchCompat).isChecked)
+        }
+    }
+
     private fun updateText() {
         binding.tvExample.text = getString(R.string.example, counter)
+    }
+
+    private fun updateLogs(checked: Boolean) {
+        val preferences = getPreferences(Context.MODE_PRIVATE)
+        preferences.edit()
+            .putBoolean("areLogsEnabled", checked)
+            .apply()
+    }
+
+    private fun logsEnabled(): Boolean {
+        val preferences = getPreferences(Context.MODE_PRIVATE)
+        return preferences.getBoolean("areLogsEnabled", false)
     }
 }
